d87e786cf0911f4cfb256130de3d2609433fbc1f
Wuicho <130885313+wuiWuicho33@users.noreply.github.com>
2024-10-23 15:09:19 -0600

    Se corrigen lso estilos de las alertas

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,3 +124,16 @@ formulario.addEventListener('submit', function(event) {
 		event.preventDefault(); // Evita que el formulario se envíe
 	}
 });
+
+/* # Alert
+---------------------------------------------- */
+function closeAlert(button) {
+	const alert = button.parentElement; // Selecciona el contenedor de alerta
+	alert.classList.add('hide');
+  
+	// Esperar a que termine la transición antes de eliminar el elemento
+	setTimeout(() => {
+	  alert.style.display = 'none';
+	}, 500);
+  }
+  
