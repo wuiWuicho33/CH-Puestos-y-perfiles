4f4479019f28169d41252c86f7057df324c80254
Wuicho <130885313+wuiWuicho33@users.noreply.github.com>
2024-10-25 11:01:49 -0600

    Reorden de scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,6 +58,29 @@ $(".sub-menu > a").click(function (event) {
 /*=========
 COMPONENTES
 =========*/
+/* # Tabs
+---------------------------------------------- */
+document.addEventListener('DOMContentLoaded', () => {
+    const tabContainers = document.querySelectorAll('.tabs-container');
+
+    tabContainers.forEach(container => {
+        const tabButtons = container.querySelectorAll('.tab-button');
+        const tabPanes = container.querySelectorAll('.tab-pane');
+
+        tabButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                // Remove 'active' class from all buttons and panes within this container
+                tabButtons.forEach(btn => btn.classList.remove('active'));
+                tabPanes.forEach(pane => pane.classList.remove('active'));
+
+                // Add 'active' class to the clicked button and corresponding pane
+                button.classList.add('active');
+                const targetId = button.dataset.target;
+                document.getElementById(targetId).classList.add('active');
+            });
+        });
+    });
+});
 
   /* # Modal
 ---------------------------------------------- */
@@ -120,30 +143,6 @@ function updateLabels() {
   
   // Inicializa la funcionalidad de los interruptores
   updateLabels();
-
-/* # Tabs
----------------------------------------------- */
-document.addEventListener('DOMContentLoaded', () => {
-    const tabContainers = document.querySelectorAll('.tabs-container');
-
-    tabContainers.forEach(container => {
-        const tabButtons = container.querySelectorAll('.tab-button');
-        const tabPanes = container.querySelectorAll('.tab-pane');
-
-        tabButtons.forEach(button => {
-            button.addEventListener('click', () => {
-                // Remove 'active' class from all buttons and panes within this container
-                tabButtons.forEach(btn => btn.classList.remove('active'));
-                tabPanes.forEach(pane => pane.classList.remove('active'));
-
-                // Add 'active' class to the clicked button and corresponding pane
-                button.classList.add('active');
-                const targetId = button.dataset.target;
-                document.getElementById(targetId).classList.add('active');
-            });
-        });
-    });
-});
 
 /* # Form
 ---------------------------------------------- */
