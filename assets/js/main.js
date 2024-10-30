e9a5b92a37f6f4fd23caad88fa474528337c11a2
Wuicho <130885313+wuiWuicho33@users.noreply.github.com>
2024-10-29 19:50:03 -0600

    Se agrega el nuevo modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
   /* # Modal
 ---------------------------------------------- */
+document.querySelectorAll('[data-modal]').forEach(button => {
+    button.addEventListener('click', function() {
+        const modalId = this.getAttribute('data-modal');
+        openModal(modalId);
+    });
+});
 
+document.querySelectorAll('[data-dismiss="modal"]').forEach(button => {
+    button.addEventListener('click', function() {
+        const modal = this.closest('.modal');
+        closeModal(modal.id);
+    });
+});
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Cerrar el modal al hacer clic fuera de él
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        closeModal(event.target.id);
+    }
+};
 
 /* # Switches
 ---------------------------------------------- */
